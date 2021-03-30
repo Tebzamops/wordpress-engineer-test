9981879152c0ede0d00652677ca0d358ad1e3e56
Tebogo Moopelwa <tebogo.moopelwa@gmail.com>
2021-03-30 17:00:09 +0200

    Added plugin called flickerleaptest

---
+++ flickerleaptest/flickerleaptest.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Flicker Leap Test
+Plugin URI: https://flickerleap.com/
+Description: Flicker Leap Assessment
+Version: 1.0
+Author: Tebogo Moopelwa
+Author URI: https://flickerleap.com/
+Text Domain: flickerleap
+*/
+
